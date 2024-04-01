480060b07fc500dca6868e2950ad3a164b6780a2
Gabriel <gmsamuel18@gmail.com>
2024-03-31 23:47:20 -0500

    day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
     let data = include_str!("../input.txt");
     println!("Part 1: {}", part1(data));
+    println!("Part 2: {}", part2(data));
 }
+
 
 fn part1(data: &str) -> i32 {
     return data
@@ -25,5 +27,43 @@ fn part1(data: &str) -> i32 {
 }
 
 fn part2(data: &str) -> i32 {
-    0
+    return data
+        .lines()
+        .map(|line| {
+            (0..line.len()).find_map(|i| get_num(line, i)).unwrap() * 10
+                + (0..line.len())
+                    .rev()
+                    .find_map(|i| get_num(line, i))
+                    .unwrap()
+        })
+        .sum::<i32>();
+}
+
+#[inline(always)]
+fn get_num(line: &str, i: usize) -> Option<i32> {
+    const NUMS: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let byte_index = line.char_indices().nth(i)?.0;
+
+    line[byte_index..]
+        .chars()
+        .next()
+        .and_then(|c| {
+            if c.is_ascii_digit() {
+                c.to_digit(10).map(|d| d as i32)
+            } else {
+                None
+            }
+        })
+        .or_else(|| {
+            NUMS.iter().enumerate().find_map(|(index, num)| {
+                if line[byte_index..].starts_with(num) {
+                    Some(index as i32 + 1)
+                } else {
+                    None
+                }
+            })
+        })
 }
