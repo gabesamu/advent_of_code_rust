a049d81fde73bc04a817c1d360e614c270a89be3
Gabriel <gmsamuel18@gmail.com>
2024-04-04 22:46:32 -0500

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let data = include_str!("../input.txt");
     println!("Day 4:");
     println!("Part 1: {}", part1(data));
-    // println!("Part 2: {}", part2(data));
+    println!("Part 2: {}", part2(data));
 }
 
 
@@ -37,4 +37,33 @@ fn part1(data: &str) -> u32 {
         })
         .sum::<u32>();
 
+}
+
+
+fn part2(data: &str) -> u32 {
+    let num_of_cards = data.lines().count();
+    let mut card_amts: Vec<u32> = vec![1; num_of_cards];
+    data
+        .lines()
+        .map(|line| {
+            let (_, nums) = line.split_once(':').unwrap();
+            let (winning, have) = nums.split_once('|').unwrap();
+
+            let winning = winning
+                .split_whitespace()
+                .collect::<Vec<&str>>();
+            have
+                .split_whitespace()
+                .filter(|&word| {
+                    winning.contains(&word)
+                })
+                .count()
+        })
+        .enumerate()
+        .for_each(|(i, num_of_winning)| {
+            for offset in 1..num_of_winning + 1 {
+                card_amts[i + offset] += card_amts[i];
+            }
+        });
+    return card_amts.iter().sum();
 }
