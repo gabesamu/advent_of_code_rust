5ed2f1272e1b7aad5228c7383b9fe5f24191fd44
Lucius <gmsamuel18@gmail.com>
2024-05-03 21:00:41 -0500

    day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,13 +4,13 @@ fn main() {
     let data = include_str!("../input.txt");
     println!("Day 8:");
     println!("{}", part1(data));
-    // println!("{}", part2(data));
+    println!("{}", part2(data));
 }
 
 pub fn speed_test() {
     let data = include_str!("../input.txt");
     part1(data);
-    // part2(data);
+    part2(data);
 }
 
 fn part1(data: &str) -> i32 {
@@ -44,4 +44,66 @@ fn part1(data: &str) -> i32 {
     }
 
     return steps as i32;
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    let mut a = a;
+    let mut b = b;
+
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+
+    return a;
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    return a * b / gcd(a, b);
+}
+
+fn part2(data: &str) -> i64 {
+    let directions = data.lines().next().unwrap();
+
+    let mut nodes = HashMap::with_capacity(800);
+    let mut start_nodes = Vec::new();
+
+    data.lines().skip(2).for_each(|line| {
+        let (node, directions) = line.split_once(" = ").unwrap();
+        let (left, right) = directions
+            .trim_matches(|c| c == '(' || c == ')')
+            .split_once(", ")
+            .unwrap();
+
+        nodes.insert(node, (left, right));
+
+        if node.ends_with("A") {
+            start_nodes.push(node);
+        }
+    });
+
+    let dir_len = directions.len();
+
+    let steps = start_nodes
+        .iter()
+        .map(|start| {
+            let mut steps = 0;
+            let mut curr = *start;
+
+            while !curr.ends_with("Z") {
+                directions.chars().for_each(|c| {
+                    let (left, right) = nodes.get(curr).unwrap();
+                    let next = if c == 'L' { left } else { right };
+                    curr = next;
+                });
+
+                steps += dir_len;
+            }
+
+            return steps as i64;
+        })
+        .fold(1, |ans, steps| lcm(ans, steps));
+
+    return steps as i64;
 }
