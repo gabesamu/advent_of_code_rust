4e0bb8d8f2872f775e3892057585fdefe8d3c624
Gabriel <gmsamuel18@gmail.com>
2024-05-04 14:55:21 -0500

    day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,8 +1,14 @@
-fn main() {
+pub fn main() {
     let data = include_str!("../input.txt");
     println!("Day 9:");
     println!("Part 1: {}", part1(data));
-    // println!("Part 2: {}", part2(data));
+    println!("Part 2: {}", part2(data));
+}
+
+pub fn speed_test() {
+    let data = include_str!("../input.txt");
+    part1(data);
+    part2(data);
 }
 
 fn part1(data: &str) -> i32 {
@@ -26,6 +32,31 @@ fn part1(data: &str) -> i32 {
             }
 
             diffs.iter().rev().fold(0, |next, diff| next + diff)
+        })
+        .sum()
+}
+
+fn part2(data: &str) -> i32 {
+    data.lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .map(|mut history| {
+            let mut diffs = Vec::new();
+            diffs.push(*history.first().unwrap());
+            while !history.iter().all(|n| *n == 0) {
+                let new_history = history
+                    .iter()
+                    .zip(history.iter().skip(1))
+                    .map(|(a, b)| b - a)
+                    .collect::<Vec<i32>>();
+                diffs.push(*new_history.first().unwrap());
+                history = new_history;
+            }
+
+            diffs.iter().rev().fold(0, |next, diff| -next + diff)
         })
         .sum()
 }
